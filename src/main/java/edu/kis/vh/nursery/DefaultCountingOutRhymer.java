40a714531fefb62f33bef87d049eb5924f4f6b2b
ipit1y <vladvoit07@gmail.com>
2026-03-23 23:58:52 +0100

    6. Dodanie constant

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    static final int CAPACITY = 12;
+    static final int TOP_INDEX = 11;
+    static final int EMPTY_RHYMER_INDICATOR = -1;
+    static final int DEFAULT_RETURN_VALUE = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[CAPACITY];
+
+    public int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_RHYMER_INDICATOR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == TOP_INDEX;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total--];
     }
 
